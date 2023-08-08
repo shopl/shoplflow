25312263bf66bdeb853b4168a639a2466f4f4cb9
velo <velo.kim@shoplworks.com>
2023-08-08 15:56:59 +0900

    chore: test ui components

---
+++ packages/core/src/Button.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Button = () => {
+  return <Wrapper>Button</Wrapper>;
+};
+
+export default Button;
