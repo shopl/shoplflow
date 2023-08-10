c4e1d5b102ffbdcde951a001a4494f35a3a2cf18
null <null>
2023-08-10 01:14:53 +0000

    Update tokens via GitHub Actions

--- packages/base/src/styles/tokens/borderRadius.ts
+++ packages/base/src/styles/tokens/borderRadius.ts
@@ -2,6 +2,7 @@
 /* eslint-disable */
 export const borderRadius = {
   borderRadius04: '4px',
+  borderRadius02: '2px',
   borderRadius06: '6px',
   borderRadius08: '8px',
   borderRadius12: '12px',
