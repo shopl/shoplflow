e89a9fd530e342edbc8f93266f8aaa3eee03126d
velo <velo.kim@shoplworks.com>
2023-08-17 13:09:41 +0900

    feat: icons

--- packages/hada-assets/scripts/build-icons.sh
+++ packages/hada-assets/scripts/build-icons.sh
@@ -2,7 +2,7 @@
 
 svgr \
   -d ./src/icons/generated/ \
-  --template ./scripts/templates/icon-template.js \
+  --template ./scripts/templates/template.js \
   --index-template ./scripts/templates/icon-index-template.js \
   ./src/icons/assets/ \
   --icon \
