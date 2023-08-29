cf17c2a597fa9f2af0c3efd8e85af1b3578ac9c1
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-08-29 11:34:24 +0900

    fix: typography tokens

--- packages/base/src/styles/index.ts
+++ packages/base/src/styles/index.ts
@@ -1,4 +1,4 @@
-import { colorTokens, spacingTokens, fontWeightTokens, borderRadiusTokens } from './tokens';
+import { colorTokens, spacingTokens, fontWeightTokens, borderRadiusTokens, typographyTokens } from './tokens';
 
 import './global.css';
 
@@ -6,6 +6,7 @@ type ColorTokens = keyof typeof colorTokens;
 type SpacingTokens = keyof typeof spacingTokens;
 type BorderRadiusTokens = keyof typeof borderRadiusTokens;
 type FontWeightTokens = keyof typeof fontWeightTokens;
+type TypographyTokens = keyof typeof typographyTokens;
 
-export type { ColorTokens, SpacingTokens, BorderRadiusTokens, FontWeightTokens };
-export { colorTokens, spacingTokens, fontWeightTokens, borderRadiusTokens };
+export type { ColorTokens, SpacingTokens, BorderRadiusTokens, FontWeightTokens, TypographyTokens };
+export { colorTokens, spacingTokens, fontWeightTokens, borderRadiusTokens, typographyTokens };
