5d4abcf27d6e31f8af08d8a1e835b5930c58ebd4
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-09-08 16:44:24 +0900

    fix: useDomain

--- packages/base/src/hooks/useDomain.ts
+++ packages/base/src/hooks/useDomain.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import type { DomainType } from '../types/Domain';
 
-type UseDarkModeProps = {
+type UseDomainProps = {
   domain?: DomainType;
 };
 
-export function useDomain({ domain = 'SHOPL' }: UseDarkModeProps) {
+export const useDomain = ({ domain = 'SHOPL' }: UseDomainProps) => {
   const [domainType, setDomainType] = useState<DomainType | undefined>(undefined);
 
   useEffect(() => {
@@ -13,25 +13,18 @@ export function useDomain({ domain = 'SHOPL' }: UseDarkModeProps) {
       setDomainType(domain);
     }
   }, [domain]);
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // const domain = localStorage.getItem('domain');
-      // setDomainType(domain ? (domain as DomainType) : 'SHOPL');
-    }
-  }, []);
 
   useEffect(() => {
     if (!domainType) {
-      return;
+      throw new Error('domainType is undefined');
     }
     document.documentElement.dataset.shoplflow = domainType;
     if (domainType === 'SHOPL') {
       document.documentElement.dataset.shoplflow = 'shopl';
-      // localStorage.setItem('domain', domainType);
-    } else {
+    }
+    if (domainType === 'HADA') {
       document.documentElement.dataset.shoplflow = 'hada';
-      // localStorage.setItem('domain', domainType);
     }
   }, [domainType]);
   return null;
-}
+};
