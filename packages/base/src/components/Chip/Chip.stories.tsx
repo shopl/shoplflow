0d168146dba95a28a7671953f82132ff3a7c0432
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-09-18 15:50:14 +0900

    feat: add Chip layout

--- packages/base/src/components/Chip/Chip.stories.tsx
+++ packages/base/src/components/Chip/Chip.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import type { Meta, StoryFn } from '@storybook/react';
+import type { StoryFn } from '@storybook/react';
 import { Stack } from '../Stack';
 import Chip from './Chip';
 import type { ChipProps } from './Chip.types';
@@ -8,9 +8,9 @@ import type { ChipProps } from './Chip.types';
 export default {
   title: 'COMPONENTS/Chip',
   component: Chip,
-} as Meta;
+};
 
-export const Template: StoryFn<ChipProps> = (args) => {
+export const Playground: StoryFn<ChipProps> = (args) => {
   return (
     <Stack>
       <Stack>
--- packages/base/src/components/Chip/Chip.styled.ts
+++ packages/base/src/components/Chip/Chip.styled.ts
@@ -1,3 +1,7 @@
 import styled from '@emotion/styled';
 
-export const StyledChip = styled.div``;
+export const StyledChip = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
