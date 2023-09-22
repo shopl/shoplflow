df11b0dad42541b5cac7578745dcb435cea65946
velo <velo.kim@shoplworks.com>
2023-09-22 11:06:11 +0900

    update: base/commponent Button

--- packages/base/src/components/Button/Button.tsx
+++ packages/base/src/components/Button/Button.tsx
@@ -1,40 +1,13 @@
 import { forwardRef } from 'react';
-import { getDomain } from '../../hooks';
-import type { TypographyTokens } from 'src/styles';
 import { Text } from '../Text';
 import { StyledButton } from './Button.styled';
-import type { ButtonComponent, ButtonProps, ButtonSizeVar } from './Button.types';
-import type { DomainType } from 'src/types/Domain';
-
-const makeTypo = (domain: Lowercase<DomainType>, size: ButtonSizeVar): TypographyTokens => {
-  if (domain === 'shopl' && size === 'm') {
-    return 'body1_400';
-  }
-
-  if (domain === 'shopl' && size === 's') {
-    return 'body2_400';
-  }
-
-  if (domain === 'hada' && size === 'm') {
-    return 'body1_500';
-  }
-
-  if (domain === 'hada' && size === 's') {
-    return 'body2_500';
-  }
-
-  return 'body1_400';
-};
+import type { ButtonComponent, ButtonProps } from './Button.types';
 
 const Button: ButtonComponent = forwardRef(
   <T extends React.ElementType = 'button'>(
     { styleVar, sizeVar = 'm', style, color, as, children, leftChildren, rightChildren, ...rest }: ButtonProps<T>,
     ref: React.ComponentPropsWithRef<T>['ref'],
   ) => {
-    const domain = getDomain();
-
-    const typography: TypographyTokens = makeTypo(domain, sizeVar);
-
     return (
       <StyledButton
         styleVar={styleVar}
@@ -47,7 +20,10 @@ const Button: ButtonComponent = forwardRef(
         {...rest}
       >
         {leftChildren}
-        <Text color={styleVar === 'primary' ? 'neutral0' : 'neutral700'} typography={typography}>
+        <Text
+          color={styleVar === 'primary' ? 'neutral0' : 'neutral700'}
+          typography={sizeVar === 'm' ? 'body1_400' : 'body2_400'}
+        >
           {children}
         </Text>
         {rightChildren}
