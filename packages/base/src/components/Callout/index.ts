a125a9440168a085506828f84c208c134e5f2fff
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-10-10 08:50:32 +0900

    fix: index

--- packages/base/src/components/Callout/index.ts
+++ packages/base/src/components/Callout/index.ts
@@ -1,4 +1,3 @@
 export { default as Callout } from './Callout';
 export * from './Callout';
 export * from './Callout.types';
-export * from './Callout.styled';
--- packages/base/src/components/Chips/ChipButton/index.ts
+++ packages/base/src/components/Chips/ChipButton/index.ts
@@ -1,3 +1,2 @@
 export { default as ChipButton } from './ChipButton';
-export * from './ChipButton.styled';
 export * from './ChipButton.types';
