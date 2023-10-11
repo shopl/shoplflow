d6dad870f12d7d6607f9596b3ea4baf82d792d8b
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-10-11 09:17:49 +0900

    fix: debug modal#3

--- packages/base/build.js
+++ packages/base/build.js
@@ -4,40 +4,40 @@ import { dtsPlugin } from 'esbuild-plugin-d.ts';
 
 import babel from 'esbuild-plugin-babel';
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const _dirname = path.dirname(fileURLToPath(import.meta.url));
-const packageJson = JSON.parse(fs.readFileSync(path.resolve(_dirname, './package.json'), 'utf8'));
-const { peerDependencies, dependencies } = packageJson;
+// const _dirname = path.dirname(fileURLToPath(import.meta.url));
+// const packageJson = JSON.parse(fs.readFileSync(path.resolve(_dirname, './package.json'), 'utf8'));
+// const { peerDependencies, dependencies } = packageJson;
 
-const externals = () => {
-  if (peerDependencies && dependencies) {
-    return Object.keys(dependencies).concat(Object.keys(peerDependencies));
-  }
-  if (peerDependencies) {
-    return Object.keys(peerDependencies);
-  }
-  // console.log(dependencies);
-  if (dependencies) {
-    return Object.keys(dependencies);
-  }
-  return [];
-};
+// const externals = () => {
+//   if (peerDependencies && dependencies) {
+//     return Object.keys(dependencies).concat(Object.keys(peerDependencies));
+//   }
+//   if (peerDependencies) {
+//     return Object.keys(peerDependencies);
+//   }
+//   // console.log(dependencies);
+//   if (dependencies) {
+//     return Object.keys(dependencies);
+//   }
+//   return [];
+// };
 
 const buildConfig = {
   entryPoints: ['src/index.ts'],
   outdir: 'dist',
-  treeShaking: true,
+  treeShaking: false,
   bundle: true,
-  minify: true,
+  minify: false,
   splitting: false,
   metafile: true,
   write: true,
   logLevel: 'debug',
   tsconfig: '../../tsconfig.esbuild.json',
-  external: externals(),
+  // external: externals(),
   plugins: [dtsPlugin(), babel()],
 };
 
