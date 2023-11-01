3d6ab902bdd9b7601750cb923b6057add21749c0
Jason <jasonjeong.dev@gmail.com>
2023-11-01 16:10:13 +0900

    fix: tsup config options

--- packages/base/tsup.config.js
+++ packages/base/tsup.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/**/*.{ts,tsx,js,jsx}', 'src/styles/global.css', 'src/styles/reset.css'],
-  splitting: false,
+  splitting: true,
   sourcemap: true,
+  treeshake: 'safest',
   clean: true,
   format: ['esm', 'cjs'],
   dts: true,
