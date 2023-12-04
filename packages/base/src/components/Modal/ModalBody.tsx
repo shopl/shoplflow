f16b87a8f3640d3405834fd87870c6f1c4595014
Jason <jasonjeong.dev@gmail.com>
2023-12-04 10:25:30 +0900

    fix: selects

--- packages/base/src/components/Modal/ModalBody.tsx
+++ packages/base/src/components/Modal/ModalBody.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { BodyContainer, ModalBodyContainerInner, ModalBodyContent } from './Modal.styled';
 import type { ModalBodyProps } from './Modal.types';
 import { useResizeObserver } from '@shoplflow/utils';
@@ -11,9 +11,11 @@ const ModalBody = ({
   sizeVar,
   height: modalContainerHeight,
 }: ModalBodyProps) => {
-  const { height: windowHeight } = useResizeObserver(document.body, {
+  const { height } = useResizeObserver(document.body, {
     trackHeight: true,
   });
+
+  const [windowHeight, setWindowHeight] = useState(0);
 
   const headerHeight = 64;
   const footerHeight = 64;
@@ -43,6 +45,12 @@ const ModalBody = ({
   const heightUnderMaxHeight = windowHeight - topBottomMargin - headerFooterHeight;
 
   const heightOverMaxHeight = 1200 - topBottomMargin - headerFooterHeight;
+
+  useEffect(() => {
+    if (height) {
+      setWindowHeight(window.innerHeight);
+    }
+  }, [height]);
 
   return (
     <BodyContainer isIncludeHeader={isIncludeHeader} height={setAutoHeightMin()}>
--- packages/utils/src/hooks/useSelect.ts
+++ packages/utils/src/hooks/useSelect.ts
@@ -1,17 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 /**
  * select 기능을 추상화한 hook입니다.
- * - mode에 따라 single select와 multi select를 지원합니다.
+ *
+ * mode에 따라 single select와 multi select를 지원합니다.
  *
  * MULTI 모드에서는 max 옵션을 통해 최대 선택 가능한 개수를 설정할 수 있습니다.
  * 기본값은 무제한입니다.
  *
  * ### Type
- * > `handleToggleSelect: (item: T, id?: string) => void;`
+ * > `handleToggleSelect: (id?: string) => void;`
  * - id는 객체에서 특정 ID를 통해 비교할 수 있습니다.
- * - id를 넣지 않을 경우, item을 그대로 비교합니다.
- * - handleToggleSelect(item, 'someId');
+ * - handleToggleSelect('someId');
  *
  * > `setSelectedItem: (item: T) => void;`
  * - setSelectedItem은 선택된 item을 설정합니다.
@@ -27,20 +27,28 @@ import { useState, useEffect } from 'react';
  * import {useSelected} from "@shoplflow/utils"
  * const App = () => {
  *
- * const data = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
- * const { selectedItem, handleToggleSelect } = useSelected<string>(data, 'MULTI');
+ * const data = new Array(10).fill(0).map((_, index) => {
+ *   return {
+ *     label: `label${index}`,
+ *     value: `value${index}`,
+ *   };
+ * });
+ *
+ * const { selectedItem, handleToggleSelect } = useSelected('MULTI', data, { key: 'value' });
  *
  * return (
- *  <Dropdown
+ * <Dropdown
  *   {...args}
  *   option={'CLICK'}
  *   width={'100%'}
- *   trigger={<Dropdown.Button placeholder={'값이 없어요'} value={selectedItem.map((data) => data).join(',')} />}
+ *   trigger={
+ *     <Dropdown.Button placeholder={'값이 없어요'} value={selectedItem.map((data) => data.value).join(',')} />
+ *   }
  *   popper={
  *     <Dropdown.Content type={'FILL'}>
  *       {data.map((item) => (
- *         <Text key={item} onClick={() => handleToggleSelect(item)}>
- *           {item}
+ *         <Text key={item.value} onClick={() => handleToggleSelect(item.value)}>
+ *           {item.label}
  *         </Text>
  *       ))}
  *     </Dropdown.Content>
@@ -53,24 +61,32 @@ import { useState, useEffect } from 'react';
  * import {useSelected} from "@shoplflow/utils"
  * const App = () => {
  *
- * const data = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
- * const { selectedItem, handleToggleSelect } = useSelected<string>(data, 'SINGLE');
+ * const data = new Array(10).fill(0).map((_, index) => {
+ *   return {
+ *     label: `label${index}`,
+ *     value: `value${index}`,
+ *   };
+ * });
+ *
+ * const { selectedItem, handleToggleSelect } = useSelected('SINGLE', data, { key: 'value' });
  *
  * return (
  * <Dropdown
- *  {...args}
- *  option={'CLICK'}
- *  width={'100%'}
- *  trigger={<Dropdown.Button placeholder={'값이 없어요'} value={selectedItem || ''} />}
- *  popper={
- *  <Dropdown.Content type={'FILL'}>
- *    {data.map((item) => (
- *    <Text key={item} onClick={() => handleToggleSelect(item)}>
- *      {item}
- *    </Text>
- *    ))}
- *  </Dropdown.Content>
- *  }
+ *   {...args}
+ *   option={'CLICK'}
+ *   width={'100%'}
+ *   trigger={
+ *     <Dropdown.Button placeholder={'값이 없어요'} value={selectedItem.map((data) => data.value).join(',')} />
+ *   }
+ *   popper={
+ *     <Dropdown.Content type={'FILL'}>
+ *       {data.map((item) => (
+ *         <Text key={item.value} onClick={() => handleToggleSelect(item.value)}>
+ *           {item.label}
+ *         </Text>
+ *       ))}
+ *     </Dropdown.Content>
+ *   }
  * />
  * )}
  *```
@@ -78,114 +94,102 @@ import { useState, useEffect } from 'react';
 
 type SelectedItem<T, Mode extends 'SINGLE' | 'MULTI'> = Mode extends 'SINGLE' ? T | null : T[];
 
-// 'SINGLE' 모드에 대한 오버로드 시그니처
 export function useSelect<T>(
-  initialItems: T[],
   mode: 'SINGLE',
+  data: T[],
+  options?: { key: string; defaultSelected?: T[] },
 ): {
-  items: T[];
   selectedItem: SelectedItem<T, 'SINGLE'>;
-  setSelectedItem: (item: T) => void;
-  handleToggleSelect: (item: T, id?: string) => void;
+  handleToggleSelect: (id: string) => void;
+  handleSelect: (id: string) => void;
+  handleReset: () => void;
+  handleRemove: (id: string) => void;
 };
 
-// 'MULTI' 모드에 대한 오버로드 시그니처
 export function useSelect<T>(
-  initialItems: T[],
   mode: 'MULTI',
-  max?: number,
+  data: T[],
+  options?: { key: string; max?: number; defaultSelected?: T[] },
 ): {
-  items: T[];
   selectedItem: SelectedItem<T, 'MULTI'>;
-  setSelectedItem: (item: T) => void;
-  handleToggleSelect: (item: T, id?: string) => void;
+  handleToggleSelect: (id: string) => void;
+  handleSelect: (id: string) => void;
+  handleReset: () => void;
+  handleRemove: (id: string) => void;
 };
 
-export function useSelect<T, Mode extends 'SINGLE' | 'MULTI'>(
-  initialItems: T[],
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useSelect<T extends { [key: string]: any }, Mode extends 'SINGLE' | 'MULTI'>(
   mode: Mode,
-  max?: number,
+  data: T[],
+  options?: { key: string; max?: number; defaultSelected?: T[] },
 ): {
-  items: T[];
-  selectedItem: SelectedItem<T, Mode>;
-  setSelectedItem: (item: T) => void;
-  handleToggleSelect: (item: T, id?: string) => void;
+  selectedItem: T[] | T | null;
+  handleSelect: (id: string) => void;
+  handleRemove: (id: string) => void;
+  handleReset: () => void;
+  handleToggleSelect: (id: string) => void;
 } {
-  const [items] = useState<T[]>(initialItems);
-  const [selectedItem, setSelectedItem] = useState<T[]>([]);
+  if (!data) {
+    throw new Error('data는 필수 값입니다.');
+  }
+  if (options?.key && !data[0]?.[options.key]) {
+    throw new Error('입력받은 key가 data에 존재하지 않습니다. 올바른 key를 입력해주세요.');
+  }
+  if (options?.max && options.max < 1) {
+    throw new Error('max는 양수여야 합니다.');
+  }
 
-  const handleSelect = (item: T) => {
-    if (mode === 'MULTI') {
-      if (max && selectedItem.length >= max) {
-        return;
-      }
+  const [selectedItem, setSelectedItem] = useState<T[]>(options?.defaultSelected ?? []);
+  const key = options?.key || 'id';
+
+  // 아이디를 이용해 데이터 배열에서 아이템 찾는 함수
+  const findItemById = (id: string): T | undefined => {
+    return data.find((item) => item[key] === id);
+  };
+
+  // 아이템 선택 함수
+  const handleSelect = (id: string) => {
+    const newItem = findItemById(id);
+    if (newItem) {
       setSelectedItem((currentSelected) => {
-        if (currentSelected.includes(item)) {
-          return currentSelected.filter((selected) => selected !== item);
+        if (mode === 'MULTI') {
+          return [...currentSelected, newItem];
         } else {
-          return [...currentSelected, item];
+          return [newItem];
         }
       });
     }
-    if (mode === 'SINGLE') {
-      setSelectedItem([item]); // TypeScript 오류를 피하기 위해 any로 캐스
-    }
   };
 
-  // 객체에서 ID 값을 추출하는 함수
-  function extractItemId(item: T, id?: string): string {
-    if (id && typeof item === 'object' && item !== null && id in item) {
-      return String(item[id]);
-    }
-    return String(item);
-  }
-
-  // 'SINGLE' 모드에서의 선택 처리
-  function handleSingleSelect(currentSelected: T[], item: T, itemId: string, id?: string): T[] {
-    const isSelected = id ? currentSelected[0]?.[id] === itemId : currentSelected[0] === item;
-    return isSelected ? [] : [item];
-  }
-
-  // 'MULTI' 모드에서의 선택 처리
-  function handleMultiSelect(currentSelected: T[], item: T, itemId: string, id?: string): T[] {
-    const index = id
-      ? currentSelected.findIndex((selected) => typeof selected === 'object' && selected && selected[id] === itemId)
-      : currentSelected.findIndex((selected) => selected === item);
-
-    if (index !== -1) {
-      return [...currentSelected.slice(0, index), ...currentSelected.slice(index + 1)];
-    } else {
-      return [...currentSelected, item];
-    }
-  }
+  // 아이템 제거 함수
+  const handleRemove = (id: string) => {
+    setSelectedItem((currentSelected) => currentSelected.filter((item) => item[key] !== id));
+  };
 
   // 메인 토글 함수
-  const handleToggleSelect = (item: T, id?: string) => {
-    const itemId = extractItemId(item, id);
-
-    if (mode === 'SINGLE') {
-      setSelectedItem((currentSelected) => handleSingleSelect(currentSelected, item, itemId, id));
-    } else if (mode === 'MULTI') {
-      setSelectedItem((currentSelected) => handleMultiSelect(currentSelected, item, itemId, id));
+  const handleToggleSelect = (id: string) => {
+    const isSelected = selectedItem.find((item) => item[key] === id);
+    if (isSelected) {
+      if (options?.max && selectedItem.length >= options.max) {
+        return;
+      }
+      handleRemove(id);
+    }
+    if (!isSelected) {
+      handleSelect(id);
     }
   };
 
-  const getSelectedItem = <T, Mode extends 'SINGLE' | 'MULTI'>(item: T[], mode: Mode) => {
-    if (mode === 'MULTI') {
-      return item as SelectedItem<T, Mode>;
-    } else {
-      return item[0] as SelectedItem<T, Mode>;
-    }
-  };
-
-  useEffect(() => {
+  const handleReset = () => {
     setSelectedItem([]);
-  }, [items]);
+  };
 
   return {
-    items,
-    selectedItem: getSelectedItem(selectedItem, mode),
-    setSelectedItem: handleSelect,
+    selectedItem: mode === 'MULTI' ? selectedItem : selectedItem[0] || null,
+    handleReset,
     handleToggleSelect,
+    handleSelect,
+    handleRemove,
   };
 }
