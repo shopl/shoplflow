f2e4801ca215114c27cb12d5012c9493739efd56
Jason <jason.jeong@shoplworks.com>
2024-01-04 16:35:33 +0900

    fix: SelectInputButton value type 수정 (#190)

--- packages/base/src/components/Buttons/IconButton/IconButton.tsx
+++ packages/base/src/components/Buttons/IconButton/IconButton.tsx
@@ -1,13 +1,22 @@
-import type { ComponentPropsWithRef, ElementType } from 'react';
-import { forwardRef } from 'react';
+import type { ComponentPropsWithRef, ElementType, MouseEvent } from 'react';
+import { useState, forwardRef } from 'react';
 import { StyledIconButton } from './IconButton.styled';
 import type { IconButtonProps, IconButtonComponent } from './IconButton.types';
 
 const IconButton: IconButtonComponent = forwardRef(
   <T extends ElementType = 'button'>(
-    { styleVar, sizeVar, disabled, children, color, ...rest }: IconButtonProps<T>,
+    { styleVar, sizeVar, disabled, children, color, onMouseEnter, onMouseLeave, ...rest }: IconButtonProps<T>,
     ref: ComponentPropsWithRef<T>['ref'],
   ) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const handleOnMouseEnter = (e: MouseEvent<HTMLButtonElement>) => {
+      setIsHovered(true);
+      onMouseEnter && onMouseEnter(e);
+    };
+    const handleOnMouseLeave = (e: MouseEvent<HTMLButtonElement>) => {
+      setIsHovered(false);
+      onMouseLeave && onMouseLeave(e);
+    };
     return (
       <StyledIconButton
         styleVar={styleVar}
@@ -15,6 +24,9 @@ const IconButton: IconButtonComponent = forwardRef(
         disabled={disabled}
         color={color}
         ref={ref}
+        onMouseEnter={handleOnMouseEnter}
+        onMouseLeave={handleOnMouseLeave}
+        isHovered={isHovered}
         {...rest}
         data-shoplflow={'IconButton'}
       >
