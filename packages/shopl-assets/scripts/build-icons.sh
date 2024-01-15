6446bf189a78e29d7d30dbc99a32b63fd1738d86
Jason <jasonjeong.dev@gmail.com>
2024-01-16 08:33:55 +0900

    fix: fix script

--- packages/shopl-assets/scripts/build-icons.sh
+++ packages/shopl-assets/scripts/build-icons.sh
@@ -5,7 +5,7 @@ svgr \
   --template ./scripts/templates/template.js \
   --index-template ./scripts/templates/icon-index-template.js \
   ./src/icons/assets/ \
-  --typescript \
   --no-dimensions \
+  --typescript
 
 
