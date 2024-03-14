33f1bb57dbf12adcc5632e561441999622b777e8
Jason <jasonjeong.dev@gmail.com>
2024-03-14 11:17:32 +0900

    feat: add StackContainer

---
+++ packages/base/src/components/StackContainer/Stack.types.ts
@@ -0,0 +1,78 @@
+import type { ComponentPropsWithRef, CSSProperties, ReactElement } from 'react';
+
+import type { CustomDomComponent } from 'framer-motion';
+import type { BorderRadiusTokens, ColorTokens, SpacingTokens } from '../../styles';
+import type {
+  ChildrenProps,
+  HTMLPropsWithoutRef,
+  RenderConfigProps,
+  StringElementType,
+} from '../../utils/type/ComponentProps';
+
+export type StackGenericProps<T extends StringElementType = 'div'> = RenderConfigProps &
+  StackContainerProps &
+  HTMLPropsWithoutRef<T>;
+
+export type StackComponentType = <T extends StringElementType = 'div'>(
+  props: StackGenericProps<T> & Pick<ComponentPropsWithRef<T>, 'ref'>,
+) => ReactElement | null;
+
+export type MotionStackComponentType<T extends StringElementType = 'div'> = CustomDomComponent<
+  RenderConfigProps & HTMLPropsWithoutRef<T> & StackContainerProps
+>;
+
+export interface StackContainerProps extends StackContainerOptionProps, ChildrenProps {}
+export interface StackContainerOptionProps {
+  /**
+   * 요소들의 align-items 값
+   */
+  align?: CSSProperties['alignItems'];
+  /**
+   * 요소들의 justify-content 값
+   */
+  justify?: CSSProperties['justifyContent'];
+  /**
+   * 요소들의 flex-direction 값
+   * (값: row, column)
+   */
+  direction?: CSSProperties['flexDirection'];
+  /**
+   * 요소들 사이의 간격
+   */
+  spacing?: SpacingTokens;
+  /**
+   * flex-wrap 설정
+   */
+  flexWrap?: CSSProperties['flexWrap'];
+
+  /**
+   * width 설정
+   */
+  width?: CSSProperties['width'];
+  maxWidth?: CSSProperties['maxWidth'];
+  minWidth?: CSSProperties['minWidth'];
+  /**
+   * height 설정
+   */
+  height?: CSSProperties['height'];
+  maxHeight?: CSSProperties['maxHeight'];
+  minHeight?: CSSProperties['minHeight'];
+
+  /**
+   * padding, margin 설정
+   */
+  padding?: CSSProperties['padding'];
+  margin?: CSSProperties['margin'];
+  /**
+   * flex 설정
+   */
+  flex?: CSSProperties['flex'];
+  /**
+   * background 설정
+   */
+  background?: ColorTokens;
+  /**
+   * border-radius 설정
+   */
+  radius?: BorderRadiusTokens;
+}
---
+++ packages/base/src/components/StackContainer/index.ts
@@ -0,0 +1,5 @@
+// SHOPLFLOW_DOCS_IGNORE
+export { default as Stack } from './Stack';
+export * from './Stack';
+export * from './Stack.types';
+export * from './Stack.styled';
