9398bec130c5d48196acf1348e08e51ff32b1213
velo <velo.kim@shoplworks.com>
2024-07-05 11:17:10 +0900

    update: 버튼 tag 기반 컴포넌트 type 변경

--- packages/base/src/components/ControlButtons/Radio/Radio.types.ts
+++ packages/base/src/components/ControlButtons/Radio/Radio.types.ts
@@ -1,9 +1,9 @@
 import type { DefaultSelectedProps, DisableProps, SelectedProps } from '../../../utils/type/ComponentProps';
-import type { HTMLAttributes } from 'react';
+import type { ButtonHTMLAttributes } from 'react';
 
 export interface RadioProps extends RadioOptionProps {}
 export interface RadioOptionProps
   extends SelectedProps,
     DefaultSelectedProps,
     DisableProps,
-    HTMLAttributes<HTMLButtonElement> {}
+    ButtonHTMLAttributes<HTMLButtonElement> {}
