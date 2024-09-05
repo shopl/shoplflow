a7ebd39749f67ffebbc9e9756427d782b8fe458b
ayaan <ayaan.sung@shoplworks.com>
2024-09-05 15:06:43 +0900

    feat: Title 컴포넌트 생성

---
+++ packages/base/src/components/Title/Title.styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { colorTokens, fontWeightTokens } from '../../styles';
+
+export const Required = styled.div`
+  color: ${colorTokens.red300};
+  font-weight: ${fontWeightTokens.fontWeightBold};
+  font-size: 14px;
+  height: 20px;
+`;
---
+++ packages/base/src/components/Title/Title.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import * as Styled from './Title.styled';
+import { HelpIcon } from '@shoplflow/shopl-assets';
+import { Stack } from '../Stack';
+import { StackContainer } from '../StackContainer';
+import { Text } from '../Text';
+import { Tooltip } from '../Tooltip';
+import type { TitleProps } from './Title.types';
+
+const Title = ({
+  title,
+  titleTypography = 'body1_700',
+  titleColor,
+  description,
+  tooltipPlacement = 'right',
+  isRequired,
+  showHelpIcon,
+  tooltipOffsetValue,
+  tooltipMessage = '',
+  rightSource,
+}: TitleProps) => {
+  return (
+    <Stack.Vertical width={'100%'}>
+      <StackContainer direction={'row'} align={'center'} width={'100%'} height={'40px'} justify={'space-between'}>
+        {/* 제목 영역 */}
+        <Stack.Horizontal align='center' spacing={'spacing04'}>
+          <Text color={titleColor} typography={titleTypography}>
+            {title}
+          </Text>
+          {isRequired && <Styled.Required>*</Styled.Required>}
+          {showHelpIcon && (
+            <Tooltip
+              placement={tooltipPlacement}
+              offset={tooltipOffsetValue}
+              trigger={<HelpIcon width={20} height={20} />}
+              popper={<Tooltip.Content content={tooltipMessage} />}
+            />
+          )}
+        </Stack.Horizontal>
+        {/* 아이콘 영역 */}
+        {rightSource && rightSource}
+      </StackContainer>
+      {/* 설명 영역 */}
+      {description && (
+        <StackContainer height={'30px'}>
+          <Text typography={'body1_400'} color={'neutral500'}>
+            {description}
+          </Text>
+        </StackContainer>
+      )}
+    </Stack.Vertical>
+  );
+};
+
+export default Title;
