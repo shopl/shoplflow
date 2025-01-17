875fa515543fbc7afffc78374d9c0e319f1b5c2b
velo <velo.kim@shoplworks.com>
2025-01-17 11:22:57 +0900

    feat: Title component 생성(templates)

--- packages/templates/src/components/Title/Title.stories.tsx
+++ packages/templates/src/components/Title/Title.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import type { StoryFn } from '@storybook/react';
-import { Stack } from '@shoplflow/base';
+import { Button, colorTokens, Stack, Switch } from '@shoplflow/base';
 import Title from './Title';
 import type { TitleProps } from './Title.types';
 
@@ -10,14 +10,174 @@ export default {
   component: Title,
 };
 
-export const Playground: StoryFn<TitleProps> = (args) => {
+export const Playground: StoryFn<TitleProps> = () => {
   return (
-    <Stack>
-      <Title {...args} />
-    </Stack>
+    <Stack.Horizontal width='600px'>
+      <Title>
+        <Title.HeaderBox>
+          <Title.Header
+            depth={1}
+            title='Title'
+            isRequired={true}
+            count='00'
+            helpIconProps={{
+              tooltipPlacement: 'right',
+              tooltipOffsetValue: 4,
+              tooltipMessage: 'Tooltip 노출',
+              onClick: () => {
+                return;
+              },
+            }}
+          />
+          <Title.Actions>
+            <Button styleVar='SECONDARY' sizeVar='S'>
+              Button
+            </Button>
+            <div
+              style={{ width: '20px', height: '20px', backgroundColor: `${colorTokens.shopl100}`, marginInline: '4px' }}
+            />
+            <Switch activeColor='primary300' />
+          </Title.Actions>
+        </Title.HeaderBox>
+        <Title.Description description='paragraph1' />
+      </Title>
+    </Stack.Horizontal>
   );
 };
 
-Playground.args = {
-  title: 'Hello, Shoplflow!',
+Playground.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/design/KBxc4vIDtpSu2JlE4tKYIx/%5BShopl-Flow%5D-Shopl-%26-Hada-%EC%9B%B9-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EA%B3%B5%ED%86%B5%ED%99%94?node-id=5699-10352&m=dev',
+  },
+};
+
+export const Depth2: StoryFn<TitleProps> = () => {
+  return (
+    <Stack.Horizontal width='600px'>
+      <Title>
+        <Title.HeaderBox>
+          <Title.Header
+            depth={2}
+            title='Title'
+            isRequired={true}
+            count='00'
+            helpIconProps={{
+              tooltipPlacement: 'right',
+              tooltipOffsetValue: 4,
+              tooltipMessage: 'Tooltip 노출',
+              onClick: () => {
+                return;
+              },
+            }}
+          />
+          <Title.Actions>
+            <Button styleVar='SECONDARY' sizeVar='S'>
+              Button
+            </Button>
+            <div
+              style={{ width: '20px', height: '20px', backgroundColor: `${colorTokens.shopl100}`, marginInline: '4px' }}
+            />
+            <Switch activeColor='primary300' />
+          </Title.Actions>
+        </Title.HeaderBox>
+        <Title.Description description='paragraph1' />
+      </Title>
+    </Stack.Horizontal>
+  );
+};
+
+export const Depth3: StoryFn<TitleProps> = () => {
+  return (
+    <Stack.Horizontal width='600px'>
+      <Title>
+        <Title.HeaderBox>
+          <Title.Header
+            depth={3}
+            title='Title'
+            isRequired={true}
+            count='00'
+            helpIconProps={{
+              tooltipPlacement: 'right',
+              tooltipOffsetValue: 4,
+              tooltipMessage: 'Tooltip 노출',
+              onClick: () => {
+                return;
+              },
+            }}
+          />
+          <Title.Actions>
+            <Button styleVar='SECONDARY' sizeVar='S'>
+              Button
+            </Button>
+            <div
+              style={{ width: '20px', height: '20px', backgroundColor: `${colorTokens.shopl100}`, marginInline: '4px' }}
+            />
+            <Switch activeColor='primary300' />
+          </Title.Actions>
+        </Title.HeaderBox>
+        <Title.Description description='paragraph1' />
+      </Title>
+    </Stack.Horizontal>
+  );
+};
+
+export const WithoutActions: StoryFn<TitleProps> = () => {
+  return (
+    <Stack.Horizontal width='600px'>
+      <Title>
+        <Title.HeaderBox>
+          <Title.Header
+            depth={1}
+            title='Title'
+            isRequired={true}
+            count='00'
+            helpIconProps={{
+              tooltipPlacement: 'right',
+              tooltipOffsetValue: 4,
+              tooltipMessage: 'Tooltip 노출',
+              onClick: () => {
+                return;
+              },
+            }}
+          />
+        </Title.HeaderBox>
+        <Title.Description description='paragraph1' />
+      </Title>
+    </Stack.Horizontal>
+  );
+};
+
+export const WithoutDescription: StoryFn<TitleProps> = () => {
+  return (
+    <Stack.Horizontal width='600px'>
+      <Title>
+        <Title.HeaderBox>
+          <Title.Header
+            depth={1}
+            title='Title'
+            isRequired={true}
+            count='00'
+            helpIconProps={{
+              tooltipPlacement: 'right',
+              tooltipOffsetValue: 4,
+              tooltipMessage: 'Tooltip 노출',
+              onClick: () => {
+                return;
+              },
+            }}
+          />
+          <Title.Actions>
+            <Button styleVar='SECONDARY' sizeVar='S'>
+              Button
+            </Button>
+            <div
+              style={{ width: '20px', height: '20px', backgroundColor: `${colorTokens.shopl100}`, marginInline: '4px' }}
+            />
+            <Switch activeColor='primary300' />
+          </Title.Actions>
+        </Title.HeaderBox>
+      </Title>
+    </Stack.Horizontal>
+  );
 };
--- packages/templates/src/components/Title/Title.tsx
+++ packages/templates/src/components/Title/Title.tsx
@@ -1,8 +1,61 @@
-import { StyledTitle } from './Title.styled';
-import type { TitleProps } from './Title.types';
+import { Stack, StackContainer, Text } from '@shoplflow/base';
+import { StyledRequired, getTypographyAndColor } from './Title.styled';
+import type { ActionsProps, DescriptionProps, TitleHeaderProps, TitleProps } from './Title.types';
+import TitleHelpIcon from './TitleHelpIcon';
+import type { ChildrenProps } from '@shoplflow/base/src/utils/type/ComponentProps';
 
-const Title = ({ title }: TitleProps) => {
-  return <StyledTitle data-shoplflow={'Title'}>{title}</StyledTitle>;
+const Actions = ({ children }: ActionsProps) => {
+  return <Stack.Horizontal align='center'>{children}</Stack.Horizontal>;
 };
+
+const HeaderBox = ({ children }: ChildrenProps) => {
+  return (
+    <StackContainer.Horizontal align='center' width='100%' minHeight='40px' height='auto' justify='space-between'>
+      {children}
+    </StackContainer.Horizontal>
+  );
+};
+const Header = ({ depth, title, isRequired, count, helpIconProps }: TitleHeaderProps) => {
+  const { color, typography } = getTypographyAndColor(depth);
+  return (
+    <Stack.Horizontal align='center'>
+      <Stack.Horizontal spacing='spacing04' align='center' justify='center'>
+        <Text color={color} typography={typography} wordBreak={'break-all'}>
+          {title}
+        </Text>
+        {isRequired && <StyledRequired>*</StyledRequired>}
+        {Boolean(count) && (
+          <Text typography='body1_700' color='primary300'>
+            {count}
+          </Text>
+        )}
+      </Stack.Horizontal>
+      {helpIconProps && <TitleHelpIcon {...helpIconProps} />}
+    </Stack.Horizontal>
+  );
+};
+
+const Description = ({ description }: DescriptionProps) => {
+  return (
+    <StackContainer minHeight={'30px'} height={'auto'}>
+      <Text typography={'body1_400'} color={'neutral500'} wordBreak={'break-all'}>
+        {description}
+      </Text>
+    </StackContainer>
+  );
+};
+
+const Title = ({ children }: TitleProps) => {
+  return (
+    <Stack.Vertical width='100%' data-shoplflow={'Title'}>
+      {children}
+    </Stack.Vertical>
+  );
+};
+
+Title.HeaderBox = HeaderBox;
+Title.Header = Header;
+Title.Actions = Actions;
+Title.Description = Description;
 
 export default Title;
