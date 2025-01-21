e1709cb8bb98049850df969444a3049b5d555cc4
ayaan <ayaan.sung@shoplworks.com>
2025-01-21 09:21:04 +0900

    docs: ChipButton, ChipToggle 문서 정리

--- packages/base/src/components/Chips/ChipButton/ChipButton.stories.tsx
+++ packages/base/src/components/Chips/ChipButton/ChipButton.stories.tsx
@@ -1,13 +1,31 @@
 import ChipButton from './ChipButton';
 import { Stack } from '../../Stack';
 import type { StoryFn } from '@storybook/react';
-import type { ChipButtonProps } from './ChipButton.types';
+import { ChipButtonSizeVariants, ChipButtonStyleVariants, type ChipButtonProps } from './ChipButton.types';
 import { ComponentStage } from '../../../styles/Box';
 
 export default {
   title: 'COMPONENTS/Chips/ChipButton',
   component: ChipButton,
-  argTypes: { onClick: { action: 'clicked' } },
+  argTypes: {
+    onClick: { action: 'clicked' },
+    styleVar: {
+      control: {
+        type: 'select',
+      },
+      options: Object.values(ChipButtonStyleVariants),
+      description: 'Style variant of the ChipButton',
+      defaultValue: ChipButtonStyleVariants.LINE,
+    },
+    sizeVar: {
+      control: {
+        type: 'select',
+      },
+      options: Object.values(ChipButtonSizeVariants),
+      description: 'Size variant of the ChipButton',
+      defaultValue: ChipButtonSizeVariants.S,
+    },
+  },
 };
 
 export const Playground: StoryFn<ChipButtonProps> = (args) => {
@@ -24,4 +42,11 @@ Playground.args = {
   disabled: false,
   styleVar: 'LINE',
   sizeVar: 'S',
+};
+
+Playground.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/design/KBxc4vIDtpSu2JlE4tKYIx/%5BShopl-Flow%5D-Shopl-%26-Hada-%EC%9B%B9-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EA%B3%B5%ED%86%B5%ED%99%94?node-id=407-5279&p=f&m=dev',
+  },
 };
