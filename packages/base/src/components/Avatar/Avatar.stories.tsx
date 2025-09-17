008b3345331acd2c076c5575213ad716a69fc50c
ayaan0209 <ayaan.sung@shoplworks.com>
2025-09-17 13:30:04 +0900

    update: Avatar 컴포넌트에 onError 핸들러 추가 및 스토리 업데이트
    - Avatar 컴포넌트에 이미지 로드 실패 시 처리할 수 있는 onError 프로퍼티 추가

--- packages/base/src/components/Avatar/Avatar.stories.tsx
+++ packages/base/src/components/Avatar/Avatar.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import type { StoryFn } from '@storybook/react';
 import { Stack } from '../Stack';
@@ -6,6 +6,7 @@ import Avatar from './Avatar';
 import type { AvatarProps } from './Avatar.types';
 import { Icon } from '../Icon';
 import { LeaderLargeIcon } from '@shoplflow/shopl-assets';
+import { Text } from '../Text';
 
 export default {
   title: 'COMPONENTS/Avatar',
@@ -34,4 +35,32 @@ export const WidthBadge: StoryFn<AvatarProps> = (args) => {
 
 WidthBadge.args = {
   sizeVar: 'M',
+};
+
+export const WithOnError: StoryFn<AvatarProps> = (args) => {
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    setErrorMessage('이미지 로드에 실패했습니다.');
+    // eslint-disable-next-line no-console
+    console.log('Avatar 이미지 로드 실패:', event);
+  };
+
+  return (
+    <Stack direction='column' spacing='spacing16'>
+      <Text typography='body2_700'>onError 테스트 - 잘못된 이미지 URL 사용</Text>
+      <Stack>
+        <Avatar src='https://invalid-url-that-will-fail.com/image.jpg' onError={handleError} sizeVar='L' {...args} />
+      </Stack>
+      {errorMessage && (
+        <Text typography='caption_400' color='red300'>
+          {errorMessage}
+        </Text>
+      )}
+    </Stack>
+  );
+};
+
+WithOnError.args = {
+  sizeVar: 'L',
 };
