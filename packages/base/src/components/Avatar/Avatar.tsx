81cb23cee91d1ca3ef4a2da44ad265549c4d306d
ayaan0209 <ayaan.sung@shoplworks.com>
2025-09-17 14:30:48 +0900

    update: Avatar 컴포넌트에 fallbackUrl 프로퍼티 추가 및 스토리 수정
    - Avatar 컴포넌트에 이미지 로드 실패 시 대체 이미지를 설정할 수 있는 fallbackUrl 프로퍼티 추가
    - 스토리에서 정상 이미지와 에러 이미지 비교를 위한 새로운 스토리 추가

--- packages/base/src/components/Avatar/Avatar.tsx
+++ packages/base/src/components/Avatar/Avatar.tsx
@@ -3,12 +3,17 @@ import { StyledAvatar, StyledAvatarBadge, StyledAvatarContainer, StyledAvatarIma
 import type { AvatarProps } from './Avatar.types';
 import AvatarImageNone from '../../assets/mocks/AvatarNone.png';
 
-const Avatar = ({ src, badge, onError, ...rest }: AvatarProps): JSX.Element => {
+const Avatar = ({ src, badge, fallbackUrl = AvatarImageNone, ...rest }: AvatarProps): JSX.Element => {
   return (
     <StyledAvatarContainer>
       <StyledAvatar data-shoplflow={'Avatar'} {...rest}>
         {/*eslint-disable-next-line @typescript-eslint/no-unsafe-assignment*/}
-        <StyledAvatarImage src={(src ?? AvatarImageNone) as string} onError={onError} />
+        <StyledAvatarImage
+          src={src ?? fallbackUrl}
+          onError={(e) => {
+            e.currentTarget.src = fallbackUrl;
+          }}
+        />
       </StyledAvatar>
       <StyledAvatarBadge>{badge}</StyledAvatarBadge>
     </StyledAvatarContainer>
